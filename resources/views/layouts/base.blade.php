64a3b07d02a4c39b50f8f4ced370587e227f189c
ValentinHRoldan <valentinroldan223@gmail.com>
2024-12-21 02:06:52 -0300

    creando pagina principal

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,11 +6,22 @@
         @vite('resources/css/app.css');
         <title>@yield('tituloHead')</title>
     </head>
-    <body>
-        <nav>
-            <a href="/nosotros">Sobre Nosotros</a>
-            <a href="/">Principal</a>
-        </nav>
-        <h1 class="text-4xl font-extrabold">@yield('titulo')</h1>
+    <body class="bg-gray-100">
+        <header class="p-5 border-b bg-white shadow">
+            <div class="container mx-auto flex justify-between items-center">
+                <h1 class="text-3xl font-black">DevsTagram</h1>
+                <nav class="flex gap-2 items-center">
+                    <a href="" class="text-sm font-bold text-gray-600 uppercase">Login</a>
+                    <a href="" class="text-sm font-bold text-gray-600 uppercase">Crear Cuenta</a>
+                </nav>
+            </div>
+        </header>
+        <main class="container mx-auto mt-10">
+            <h2 class="font-black text-center text-3xl mb-10">@yield('titulo')</h2>
+            @yield('contenido')
+        </main>
+        <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+            todos los derechos blablabla
+        </footer>
     </body>
 </html>
