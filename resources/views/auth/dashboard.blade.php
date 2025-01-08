889285558f85f7a4c30bdfd0f932b9a350a159c7
ValentinHRoldan <valentinroldan223@gmail.com>
2025-01-08 01:41:34 -0300

    a

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -50,7 +50,7 @@ Perfil: {{$user->username}}
         @if($posts->count())
         <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
             @foreach($posts as $post)
-            <div>
+            <div class="overflow-hidden rounded-md">
                 <a href="{{route('post.show', ['post' => $post, 'user' => $user])}}">
                     <img src="{{ asset('uploads') . '/' . $post->imagen}}" alt="{{$post->titulo}}">
                 </a>
